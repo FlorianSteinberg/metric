ba1f4e3e013d15e946f919614e506e4a318eb687
Florian Steinberg <florian.steinberg@inria.fr>
2019-01-14 13:53:28 +0100

    infima, UniformSpace2MetricSpace and proof that it preserves continuity.

--- coquelicot.v
+++ coquelicot.v
@@ -1,7 +1,7 @@
 (* Interface of MetricSpace type with types from the Coqeulicot Hierarchy *)
 From mathcomp Require Import all_ssreflect.
 From rlzrs Require Import all_rlzrs.
-Require Import pointwise reals standard metric.
+Require Import pointwise reals standard metric infima_suprema.
 Require Import Reals Psatz.
 From Coquelicot Require Import Coquelicot.
 
@@ -49,18 +49,18 @@ Defined.
 Canonical NM2MS K V := MetricSpace.Pack (@NM2MS_class K V) V.
 Coercion NM2MS: NormedModule >-> MetricSpace.
 
-Definition MS2US_mixin (M: MetricSpace): UniformSpace.mixin_of M.
-  exists (fun x r y => d x y < r).
-  by move => x [eps eg0]; rewrite dstxx /=.
-  by move => x y e ineq; rewrite dst_sym.
-  move => x y z e e' ineq ineq'.
-  apply/Rle_lt_trans; first exact/(dst_trngl y).
-  exact/Rplus_lt_compat.
-Defined.
+Section MetricSpace2UniformSpace.
+  Definition MS2US_mixin (M: MetricSpace): UniformSpace.mixin_of M.
+    exists (fun x r y => d x y < r).
+    - by move => x [eps eg0]; rewrite dstxx /=.
+    - by move => x y e ineq; rewrite dst_sym.
+    move => x y z e e' ineq ineq'.
+    apply/Rle_lt_trans; first exact/(dst_trngl y).
+    exact/Rplus_lt_compat.
+  Defined.
+  
+  Canonical MS2US (M: MetricSpace):= UniformSpace.Pack M (MS2US_mixin M) M.
 
-Canonical MS2US (M: MetricSpace):= UniformSpace.Pack M (MS2US_mixin M) M.
-
-Section lemmas.
   Context (M N: MetricSpace).
   Local Open Scope metric_scope.
   Lemma cntp_cont (f: M -> N) x:
@@ -76,4 +76,152 @@ Section lemmas.
     apply/Rlt_le/prp/Rle_lt_trans; first exact/dst.
     rewrite /=; lra.
   Qed.
-End lemmas.
+End MetricSpace2UniformSpace.
+
+Section UniformSpace2MetricSpace.
+  Context (M: UniformSpace).
+
+  Definition distance_bounds (x y: M) := make_subset (fun eps => 0 <= eps /\ ball x eps y).
+  
+  Lemma dbnd_bbl x y: distance_bounds x y \from lower_boundeds.
+  Proof. by exists 0 => r []. Qed.
+  Notation "[1,oo)":= (make_subset (fun x => 1 <= x)).
+  
+  Lemma dom_d x y: ((distance_bounds x y) \u [1,oo)) \from dom mf_infimum.
+  Proof.
+    rewrite dom_inf.
+    split; first by exists 0 => z [[Az] | /= l1z]; lra.
+    by exists 1; right => /=; lra.
+  Qed.
+
+  Definition d_M (x y: M):= inf ((distance_bounds x y) \u [1,oo)).
+
+  Lemma d_inf x y: mf_infimum ((distance_bounds x y) \u [1,oo)) (d_M x y).
+  Proof. exact/inf_spec/dom_d. Qed.
+
+  Lemma d_pos x y: 0 <= d_M x y.
+  Proof.
+    apply/inf_geq; first exact/dom_d.
+    by move => z [/=[] | /= ]; lra.
+  Qed.
+
+  Lemma d_sym x y: d_M x y = d_M y x.
+  Proof.
+    apply/inf_eq; first exact/dom_d.
+    split => [r [[pos bll] | dst] | r lb].
+    - apply/inf_leq; first exact/dom_d.
+      by left; split; last apply/ball_sym.
+    - apply/inf_leq; first exact/dom_d.
+      by right.
+    apply/inf_geq; first exact/dom_d.
+    move => z [[le bll] | l1z]; apply/lb; last by right.
+    by left; split => //; exact/ball_sym.
+  Qed.  
+  
+  Lemma dxx x: d_M x x = 0.
+  Proof.
+    suff: 0 <= d_M x x /\ d_M x x <= 0 by lra.
+    - split; first apply/inf_geq; first exact/dom_d.
+      move => z [ [] | /=]; lra.
+    apply/cond_leq => eps eg0; rewrite Rplus_0_l /d_M.
+    apply/inf_leq; first exact/dom_d.
+    left; split; try lra.
+    by have := ball_center x (mkposreal _ eg0).
+  Qed.
+
+  Lemma d_leq x y: d_M x y <= 1.
+  Proof.
+    apply/inf_leq; first exact/dom_d.
+    by right => /=; lra.
+  Qed.
+
+  Lemma d_trngl x z y: d_M x z <= d_M x y + d_M y z.
+  Proof.
+    apply/cond_leq => eps eg0.
+    suff: d_M x z <= (d_M x y + eps/2) + (d_M y z + eps/2) by lra.
+    apply/inf_leq; first exact/dom_d.
+    case: (total_order_T (d_M x y + eps/2) 1); last by right => /=; have:= d_pos y z; lra.
+    case => [lt | ->]; last by right => /=; have := d_pos y z; lra.
+    case: (total_order_T (d_M y z + eps/2) 1); last by right => /=; have := d_pos x y; lra.
+    case => [lt' | ->]; last by right => /=; have := d_pos x y; lra.
+    left; split; first by have := d_pos x y; have := d_pos y z; lra.
+    have eps2: 0 < eps/2 by lra.
+    apply/(ball_triangle x y z).
+    - have [eps' [[[pos bll] | /= ineq] ineq']]:= (inf_approx (d_inf x y) eps2).
+      + exact/ball_le/bll/ineq'.
+      by have := d_pos x y; lra.
+    have [eps' [[[pos bll] | /= ineq] ineq']]:= (inf_approx (d_inf y z) eps2).
+    - exact/ball_le/bll/ineq'.
+    by have := d_pos y z; lra.
+  Qed.
+
+  Lemma dst_bll x y eps: 0 < eps -> d_M x y <= Rmin (eps/2) (1/4) -> ball x eps y.
+  Proof.
+    rewrite/d_M /= => eg0 dst.
+    pose A:= make_subset (fun eps => 0 <= eps /\ ball x eps y) \u [eta Rle 1].
+    have e4: 0 < Rmin (eps/2) (1/4) by apply/Rmin_pos; lra.
+    have [x' [[[_ bll] ineq | ]]]:= @inf_approx A _ (inf_spec (dom_d x y)) _ e4.
+    + apply (ball_le x x') => //; first apply/Rle_trans; first exact/ineq.
+      apply/Rle_trans; first by apply/Rplus_le_compat_r/dst.
+      by have := Rmin_l (eps/2) (1/4); lra.
+    have := Rmin_r (eps/2) (1/4) => ineq ineq' ineq''.
+    have : x' <= Rmin (eps/2) (1/4) + Rmin (eps/2) (1/4); try lra.
+    by apply/Rle_trans/Rplus_le_compat_r/dst.
+  Qed.
+
+  Lemma bll_dst:
+    (forall (x y: M), (forall eps, 0 < eps -> ball x eps y) -> x = y)
+      -> (forall x y, d_M x y = 0 -> x = y).
+  Proof.
+    move => prp x y dst.
+    apply/prp => eps eg0.
+    apply/(dst_bll eg0).
+    rewrite dst.
+    by apply/Rlt_le/Rmin_pos; lra.
+  Qed.
+  
+  Definition US2MS_mixin:
+    (forall (x y: M), (forall eps, 0 < eps -> ball x eps y) -> x = y) -> MetricSpace.mixin_of M.
+    exists d_M.
+    - exact/d_pos.
+    - exact/d_sym.
+    - exact/dxx.
+    - exact/bll_dst.
+    - exact/d_trngl.
+  Defined.
+
+  Definition US2MS (prp : forall x y, (forall eps, 0 < eps -> ball x eps y) -> x = y):=
+    MetricSpace.Pack (US2MS_mixin prp) M.
+End UniformSpace2MetricSpace.
+
+Section Continuity.
+  Context (N M: UniformSpace).
+  Hypothesis (dM_eq: forall (x y: M), (forall eps, 0 < eps -> ball x eps y) -> x = y).
+  Hypothesis (dN_eq: forall (x y: N), (forall eps, 0 < eps -> ball x eps y) -> x = y).
+  Definition met_M:= US2MS dM_eq.
+  Definition met_N:= US2MS dN_eq.
+  
+  Lemma cntp_cntp (f: N -> M) x: continuous f x <-> ((f: met_N -> met_M) \is_continuous_in x)%met.
+  Proof.
+    split => [cont eps eg0 | cont P [[eps eg0] prp]].
+    - have [ | [delta dg0] dprp]:= cont (fun y => d (f x: met_M) (y: met_M) <= eps).
+      + have eg2: 0 < eps/2 by lra.
+        exists (mkposreal _ eg2) => y bll.
+        suff : d (f x: met_M) y <= eps/2 by lra.
+        rewrite /d/=/d_M/=; apply/bnds_inf_leq.
+        * by exists 0; rewrite /= => z [[] | ]; lra.
+        by left; split; [lra | apply/bll].
+      exists (Rmin (delta/2) (1/4)).
+      split => [ | y dst]; first by apply/Rmin_pos; lra.
+      by apply/dprp/dst_bll/dst => /=; lra.
+    have e2: 0 < Rmin (eps/2) (1/4) by apply/Rmin_pos; lra.
+    have [delta [dg0 cnd]]:= cont (Rmin (eps/2) (1/4)) e2.
+    exists (mkposreal _ dg0) => y bll.    
+    have : d (f x: met_M) (f y) <= Rmin (eps/2) (1/4).
+    - apply/cnd; rewrite /d/=/d_M/=.
+      apply/inf_leq; first exact/dom_d.
+      by left; split; first lra.
+    rewrite /d/=/d_M/= => dst.
+    by apply/prp/dst_bll/dst => /=; lra.
+  Qed.
+End Continuity.
---
+++ infima_suprema.v
@@ -0,0 +1,163 @@
+(* Interface of MetricSpace type with types from the Coqeulicot Hierarchy *)
+From mathcomp Require Import all_ssreflect.
+From rlzrs Require Import all_rlzrs.
+Require Import pointwise reals standard metric.
+Require Import Reals Psatz Classical ChoiceFacts.
+From Coquelicot Require Import Coquelicot.
+
+Axiom choice: FunctionalCountableChoice_on R.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Section infima.
+  Local Open Scope metric_scope.
+  Implicit Types (A: subset R).  
+  Definition is_lower_bound A x:= forall a, a \from A -> x <= a.
+  
+  Definition lower_bounds A:= make_subset (fun x => is_lower_bound A x).
+  
+  Definition is_infimum A x := is_lower_bound A x /\ is_upper_bound (lower_bounds A) x.
+  
+  Definition mf_infimum:= make_mf is_infimum.
+  
+  Lemma inf_sing: mf_infimum \is_singlevalued.
+  Proof.
+    move => A inf inf' [bnd sup] [bnd' sup'].
+    suff: inf <= inf' /\ inf' <= inf by lra.
+    by split; [apply/sup' | apply/sup].
+  Qed.
+  
+  Definition infimum A:= iota (mf_infimum A).
+  Notation inf A := (infimum A).
+
+  Definition nonempty A:= exists a, a \from A.
+
+  Definition nonempties := make_subset nonempty.
+  
+  Definition bounded_from_below A := exists a, a \from lower_bounds A.
+
+  Definition lower_boundeds:= make_subset bounded_from_below.
+
+  Lemma dom_inf: dom mf_infimum === lower_boundeds \n nonempties.
+  Proof.
+    move => A; split => [[x [lb inf]] | [[y lb] [x Ax]]].
+    - split; first by exists x.
+      apply/not_all_not_ex => mty.
+      suff: x + 1 <= x by lra.
+      by apply/inf => y Ay; exfalso; apply/mty/Ay.
+    have := lb x Ax.
+    case => [ineq | eq]; last by exists x; split => [ | z lbz]; [rewrite -eq | exact/lbz].
+    suff /choice [xn xnprp]:
+      forall n, exists xn, lower_bounds A xn
+                           /\
+                           exists z, A z /\ d xn z <= /2^n.
+    - have xnbnd: forall n, is_lower_bound A (xn n) by move => n; have []:= xnprp n.
+      have /(fchy_lim_eff R_cmplt) [infA lmt]: xn \is_fast_Cauchy_sequence.
+      + apply cchy_eff_suff => n m nlm.
+        have [_ [z [Az dst]]]:= xnprp m.
+        have [_ [z' [Az' dst']]]:= xnprp n.
+        have := xnbnd n z Az; have := xnbnd m z' Az'.
+        by move : dst dst'; rewrite /d/=; split_Rabs; lra.
+      exists infA.
+      split => [z Az | x' lbx'].
+      * apply/lim_inc/lim_cnst/lim_eff_lim/lmt.
+        by move => n; rewrite/cnst; apply/xnbnd.
+      apply/cond_leq => eps eg0.
+      have /accf_tpmn [N [pos Nle]] : 0 < eps/2 by lra.
+      have [_ [z [Az dst]]]:= xnprp N.
+      have := lbx' z Az; have := lmt N.
+      by move: dst; rewrite /d/=; split_Rabs; lra.
+    suff prp: forall n, exists xn, lower_bounds A xn
+                                   /\
+                                   exists z, A z /\ d xn z <= (x - y)/2^n.
+    - move => n.
+      have /accf_tpmn [N [pos Nlxy]]: 0 < /(x - y) by apply/Rinv_0_lt_compat; lra.
+      have [xn [xnlb [z [Az dst]]]]:= prp (N + n)%nat.
+      exists xn; split => //.
+      exists z; split => //.
+      have lt: 0 < 2^n by apply pow_lt; lra.
+      have lt': 0< 2^N by apply/pow_lt; lra.
+      apply/Rle_trans; first exact/dst.
+      rewrite pow_add /Rdiv Rinv_mult_distr; try lra.
+      rewrite -Rmult_assoc -{2}(Rmult_1_l (/2^n)).
+      apply/Rmult_le_compat_r; first by apply/Rlt_le/Rinv_0_lt_compat.
+      rewrite -(Rinv_r (2^N)); try lra.
+      apply/Rmult_le_compat_r; first by apply/Rlt_le/Rinv_0_lt_compat.
+      rewrite -(Rinv_involutive (2^N)); try lra.
+      rewrite -(Rinv_involutive (x - y)); try lra.
+      by apply/Rinv_le_contravar; lra.
+    elim => [ | n [xn [xnlb [z [Az dst]]]]].
+    - by exists y; split; last by exists x; split; last by rewrite /d/=; split_Rabs; lra.
+    case: (classic (exists z', A z' /\ d xn z' <= (x - y)/2^n.+1)) => [ex | /not_ex_all_not nex].
+    - by exists xn.
+    exists (xn + (x-y)/2^n.+1).
+    split => [z' Az' |].
+    - have /not_and_or [nAz' | /Rnot_le_lt dst']:= nex z'; first by exfalso; apply/nAz'.
+      by have := xnlb z' Az'; move: dst'; rewrite /d/=; split_Rabs; lra.
+    have /not_and_or [nAz | /Rnot_le_lt dst']:= nex z; first by exfalso; apply/nAz.
+    exists z; split => //; have xnlz:= xnlb z Az.
+    have xn'lz: xn + (x - y) /2^n.+1 <= z.    
+    - have : 0 < (x - y) /2^n.+1 by apply/Rdiv_lt_0_compat/pow_lt; lra.
+      by rewrite /d /= in dst; move : dst' dst; rewrite /d [X in _ < X]/=; split_Rabs; lra.
+    rewrite /Rdiv (tpmn_half n) in dst.
+    by move: xn'lz dst' dst; rewrite /d /=; split_Rabs; lra.
+  Qed.
+                                        
+  Lemma inf_spec A: A \from dom mf_infimum -> mf_infimum A (inf A).
+  Proof.
+    move => [infA val].
+    apply(iota_correct (mf_infimum A)).
+    exists infA; split => // infA' val'.
+    exact/inf_sing/val'/val.
+  Qed.
+
+  Lemma inf_eq A r: A \from dom mf_infimum -> mf_infimum A r -> inf A = r.
+  Proof.
+    move => fd val.
+    exact/inf_sing/val/inf_spec.
+  Qed.
+
+  Lemma inf_leq A x: A \from dom mf_infimum -> x \from A -> inf A <= x.
+  Proof.
+    move => fd xfa.
+    have [lb _]:= inf_spec fd.
+    exact/lb.
+  Qed.
+
+  Lemma bnds_inf_leq A x: A \from lower_boundeds -> x \from A -> inf A <= x.
+  Proof.
+    move => bnd elt; apply/inf_leq => //.
+    by rewrite dom_inf; split; last exists x.
+  Qed.
+  
+  Lemma inf_geq A x: A \from dom mf_infimum -> x \from lower_bounds A -> x <= inf A.
+  Proof.
+    move => fd lb.
+    have [lbs nf]:= inf_spec fd.
+    exact/nf.
+  Qed.
+
+  Lemma ne_inf_geq A x: A \from nonempties -> x \from lower_bounds A -> x <= inf A.
+  Proof. by move => ne lb; apply/inf_geq; first by rewrite dom_inf; split => //; exists x. Qed.
+
+  Lemma inf_approx A infA: mf_infimum A infA -> 
+                      forall eps, 0 < eps -> exists x, x \from A /\ x <= infA + eps.
+  Proof.
+    move => [lb nf] eps eg0.
+    apply/not_all_not_ex => all.
+    have := nf (infA + eps).
+    suff: infA + eps <= infA by lra.
+    apply/nf => z Az.
+    have /not_and_or [nAz | /Rnot_le_lt]:= all z; try lra.
+    by exfalso; apply/nAz.
+  Qed.
+End infima.  
+Notation inf:= infimum.
+
+Section suprema.
+  Definition mf_supremum:= make_mf (fun (A: subset R) => is_lub A).
+
+  Definition supremum A:= iota (mf_supremum A).
+  Notation sup := supremum.
+End suprema.
